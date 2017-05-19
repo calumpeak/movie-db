e6c8f7265aa0bc9135420339c7bf83e1ccf3ea1a
Calum Peak <calumpeak@gmail.com>
2017-05-19 13:10:40 +0100

    Bootstrap test env

---
+++ test/confirm.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { expect } from 'chai';
+
+describe('Confirm Tests are working', () => {
+    it('should return the ultimate question of life, the universe, and everything', () => {
+        expect(40 + 2).to.equal(42);
+    });
+});
